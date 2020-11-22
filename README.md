cf5d3fc9d3f4577576128bdae71837f81003b41a
bas-kirill <people.can.you.can@gmail.com>
2020-11-22 07:04:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# spbu-semester1-arrays
+Homework on ICT at the AMCS SPbU (semester 1)
+
+Date: 22.11.2020
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+using namespace std;
+
+const int INF = 2e9;
+
+bool is_palindrome(char t[20], int i) {
+    if (i == (strlen(t) - 1) / 2)
+        return true;
+    else if (t[i] == t[strlen(t) - i - 2])
+        is_palindrome(t, i + 1);
+    else
+        return false;
+}
+
+int main() {
+    char t[20];
+    fgets(t, 20, stdin);
+    printf("%s", (is_palindrome(t, 0) == true ? "Palindrome" : "Not a palindrome"));
+    return 0;
+}
